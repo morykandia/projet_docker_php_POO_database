ed40c3147656cca10f66f18d000a7a150a193e40
morykandia <traore.mory-kandja@orange.fr>
2022-11-16 13:34:53 +0100

    ajout

--- app/index.php
+++ app/index.php
@@ -1,20 +1,52 @@
 <?php
 
-//use App\Factory\PDOFactory;
-//use App\Manager\UserManager;
+use App\Route\Route;
 
 require_once 'vendor/autoload.php';
 
+$controllerDir = dirname(__FILE__) . '/src/Controller';
+$dirs = scandir($controllerDir);
+$controllers = [];
+
+foreach ($dirs as $dir) {
+    if ($dir === "." || $dir === "..") {
+        continue;
+    }
+
+    $controllers[] = "App\\Controller\\" . pathinfo($controllerDir . DIRECTORY_SEPARATOR . $dir)['filename'];
+}
+
+$routesObj = [];
+
+foreach ($controllers as $controller) {
+    $reflection = new ReflectionClass($controller);
+    foreach ($reflection->getMethods() as $method) {
+       foreach ($method->getAttributes() as $attribute) {
+           /** @var Route $route */
+           $route = $attribute->newInstance();
+           $route->setController($controller)
+               ->setAction($method->getName());
+
+           $routesObj[] = $route;
+       }
+    }
+}
+
 $url = "/" . trim(explode("?", $_SERVER['REQUEST_URI'])[0], "/");
 
-switch ($url) {
-    case "/":
-        $controller = new \App\Controller\PostController();
-        $controller->home();
-        break;
+foreach ($routesObj as $route) {
+    if (!$route->match($url) || !in_array($_SERVER['REQUEST_METHOD'], $route->getMethods())) {
+        continue;
+    }
 
+    $controlerClassName = $route->getController();
+    $action = $route->getAction();
+    $params = $route->mergeParams($url);
 
-
-    default:
-        throw new Exception("RIEN TROUVE !");
+    new $controlerClassName($action, $params);
+    exit();
 }
+
+echo "NO MATCH";
+
+die;
--- app/src/Manager/UserManager.php
+++ app/src/Manager/UserManager.php
@@ -3,8 +3,6 @@
 namespace App\Manager;
 
 use App\Entity\User;
-use App\Factory\PDOFactory;
-use App\Interfaces\Database;
 
 class UserManager extends BaseManager
 {
@@ -23,5 +21,27 @@ class UserManager extends BaseManager
         }
 
         return $users;
+    }
+
+    public function getByUsername(string $username): ?User
+    {
+        $query = $this->pdo->prepare("SELECT * FROM User WHERE username = :username");
+        $query->bindValue("username", $username, \PDO::PARAM_STR);
+        $query->execute();
+        $data = $query->fetch(\PDO::FETCH_ASSOC);
+
+        if ($data) {
+            return new User($data);
+        }
+
+        return null;
+    }
+
+    public function insertUser(User $user)
+    {
+        $query = $this->pdo->prepare("INSERT INTO User (password, username), VALUES (:password, :username)");
+        $query->bindValue("password", $user->getHashedPassword(), \PDO::PARAM_STR);
+        $query->bindValue("username", $user->getUsername(), \PDO::PARAM_STR);
+        $query->execute();
     }
 }
